8c9b62b0207655292a35845b41208f5e1ec6b7ee
joy <joylydia@hotmail.com>
2023-03-17 15:56:51 +0800

    debug code by local

--- ruoyi-modules/okx/src/main/java/com/ruoyi/okx/domain/OkxBuyRecord.java
+++ ruoyi-modules/okx/src/main/java/com/ruoyi/okx/domain/OkxBuyRecord.java
@@ -1,5 +1,6 @@
 package com.ruoyi.okx.domain;
 
+import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.ruoyi.common.core.web.domain.CommonEntity;
 import lombok.AllArgsConstructor;
@@ -13,7 +14,7 @@ import java.math.BigDecimal;
 @NoArgsConstructor
 @AllArgsConstructor
 public class OkxBuyRecord extends CommonEntity {
-    @TableId
+    @TableId (type = IdType.AUTO)
     private Integer id;
 
     private String coin;
