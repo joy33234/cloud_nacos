47636d190bccb9a49be73f4442b66d8db86aa794
joy <joykuang66@gmail.com>
2023-03-24 21:31:51 +0800

    fix transactional

--- ruoyi-modules/okx/src/main/java/com/ruoyi/okx/business/CoinBusiness.java
+++ ruoyi-modules/okx/src/main/java/com/ruoyi/okx/business/CoinBusiness.java
@@ -1,32 +1,21 @@
 package com.ruoyi.okx.business;
 
 
-import com.alibaba.fastjson.JSONArray;
-import com.alibaba.fastjson.JSONObject;
-import com.baomidou.mybatisplus.core.conditions.Wrapper;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
-import com.google.common.collect.Lists;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.util.*;
-import java.util.stream.Collectors;
 import javax.annotation.Resource;
 
 import com.ruoyi.common.core.constant.CacheConstants;
-import com.ruoyi.common.core.constant.OkxConstants;
-import com.ruoyi.common.core.utils.HttpUtil;
 import com.ruoyi.common.redis.service.RedisService;
 import com.ruoyi.okx.domain.*;
-import com.ruoyi.okx.enums.CoinStatusEnum;
 import com.ruoyi.okx.mapper.CoinMapper;
-import com.ruoyi.okx.utils.Constant;
-import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -68,14 +57,8 @@ public class CoinBusiness extends ServiceImpl<CoinMapper, OkxCoin> {
     public boolean addCount(String coinStr, Integer accountId, BigDecimal count) {
         OkxCoin coin = findOne(coinStr);
         coin.setCount(coin.getCount().add(count));
-        return updateList(Arrays.asList(new OkxCoin[] { coin }));
+        return updateById(coin);
     }
-
-//    public List<Coin> findByAccountId(Integer accountId) {
-//        LambdaQueryWrapper<Coin> wrapper = new LambdaQueryWrapper();
-//        wrapper.eq(Coin::getAccountId, accountId);
-//        return this.coinMapper.selectList((Wrapper)wrapper);
-//    }
 
     public OkxCoin findOne(String coin) {
         LambdaQueryWrapper<OkxCoin> wrapper = new LambdaQueryWrapper();
@@ -83,23 +66,11 @@ public class CoinBusiness extends ServiceImpl<CoinMapper, OkxCoin> {
         return this.coinMapper.selectOne(wrapper);
     }
 
+    @Transactional(rollbackFor = Exception.class)
     public boolean updateList(List<OkxCoin> coins) {
         saveOrUpdateBatch(coins);
         return true;
     }
-
-//    @Transactional(rollbackFor = {Exception.class})
-//    public void updateStatus() {
-//        List<OkxCoin> coins = list();
-//        Integer riseCount = Integer.valueOf(((List)coins.stream().filter(item -> (item.isRise() == true)).collect(Collectors.toList())).size());
-//        boolean isRise = (riseCount.intValue() / (coins.size() - riseCount.intValue()) >= 1);
-//        if (isRise == true) {
-//            OkxConstants.OKX_TRADE_STATUS = 0;
-//        } else {
-//            OkxConstants.OKX_TRADE_STATUS = 1;
-//        }
-//        log.info("riseCount:{}, fallCount:{}, allCount:{},OKX_TRADE_STATUS:{}", new Object[] { riseCount, Integer.valueOf(coins.size() - riseCount.intValue()), Integer.valueOf(coins.size()), Integer.valueOf(OkxConstants.OKX_TRADE_STATUS) });
-//    }
 
 
     public List<OkxCoin> selectCoinList(OkxCoin coin){
