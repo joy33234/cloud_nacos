93c029d663c4cf4d790c731f09327aa7bd46b016
joy <joykuang66@gmail.com>
2023-03-27 17:24:12 +0800

    multi users balance

---
+++ ruoyi-modules/okx/src/main/java/com/ruoyi/okx/mapper/OkxAccountBalanceMapper.java
@@ -0,0 +1,8 @@
+package com.ruoyi.okx.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.ruoyi.okx.domain.OkxAccountBalance;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface OkxAccountBalanceMapper extends BaseMapper<OkxAccountBalance> {}
