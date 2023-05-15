306bf462352ad28343488d4aeea406d46c933b06
joy <joykuang66@gmail.com>
2023-05-15 21:48:43 +0800

    okx-同步余额

--- ruoyi-modules/okx/src/main/java/com/ruoyi/okx/business/SyncCoinBusiness.java
+++ ruoyi-modules/okx/src/main/java/com/ruoyi/okx/business/SyncCoinBusiness.java
@@ -1,10 +1,12 @@
 package com.ruoyi.okx.business;
 
+import cn.hutool.core.collection.CollectionUtil;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.ruoyi.common.core.constant.OkxConstants;
 import com.ruoyi.common.core.constant.RedisConstants;
 import com.ruoyi.common.core.enums.Status;
+import com.ruoyi.common.core.exception.ServiceException;
 import com.ruoyi.common.core.utils.DateUtil;
 import com.ruoyi.common.core.utils.HttpUtil;
 import com.ruoyi.common.redis.service.RedisLock;
@@ -17,10 +19,12 @@ import com.ruoyi.okx.params.dto.RiseDto;
 import com.ruoyi.okx.service.SettingService;
 import com.ruoyi.okx.utils.Constant;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.compress.utils.Lists;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
+import org.apache.commons.collections4.CollectionUtils;
 
 import javax.annotation.Resource;
 import java.math.BigDecimal;
@@ -28,6 +32,7 @@ import java.math.RoundingMode;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
@@ -64,43 +69,71 @@ public class SyncCoinBusiness {
     public void syncOkxBalance() {
         try {
             List<OkxAccount> accounts = accountBusiness.list().stream()
-                    .filter(item -> item.getStatus().intValue() == Status.OK.getCode()).collect(Collectors.toList());;
+                    .filter(item -> item.getStatus().intValue() == Status.OK.getCode()).collect(Collectors.toList());
             for (OkxAccount account:accounts) {
-                List<OkxAccountBalance> allBalanceList = balanceBusiness.list(new OkxAccountBalanceDO(null,null,null,account.getId(),null));
-                int pages = allBalanceList.size() / 20;
-                if (allBalanceList.size() % 20 != 0) {
-                    pages++;
-                }
+                List<OkxCoin> okxCoins = Lists.newArrayList();
+                List<OkxAccountBalance> balanceList = Lists.newArrayList();
                 Map<String, String> map = accountBusiness.getAccountMap(account);
+                int pages = 0;
+
+                List<OkxAccountBalance> DbBalanceList = balanceBusiness.list(new OkxAccountBalanceDO(null,null,null,account.getId(),null));
+                if (CollectionUtils.isEmpty(DbBalanceList)) {
+                    okxCoins = coinBusiness.selectCoinList(null);
+                    pages = getPages(okxCoins.size());
+                } else {
+                    pages = getPages(DbBalanceList.size());
+                }
+
+                String coins = "";
                 //帐户币种数量
                 for (int i = 0; i < pages; i++) {
-                    List<OkxAccountBalance> balanceList = allBalanceList.subList(i * 20, ((i + 1) * 20 <= allBalanceList.size()) ? ((i + 1) * 20) : allBalanceList.size());
-                    String coins = StringUtils.join(balanceList.stream().map(OkxAccountBalance::getCoin).collect(Collectors.toList()), ",");
-                    String str = HttpUtil.getOkx("/api/v5/account/balance?ccy=" + coins, null, map);
-                    JSONObject json = JSONObject.parseObject(str);
-                    if (json == null || !json.getString("code").equals("0")) {
-                        log.error("str:{}", str);
-                        return ;
+                    List<OkxAccountBalance> subBalanceList = Lists.newArrayList();
+                    if (CollectionUtils.isEmpty(DbBalanceList)) {
+                        List<OkxCoin> subCoinList = okxCoins.subList(i * 20, ((i + 1) * 20 <= okxCoins.size()) ? ((i + 1) * 20) : okxCoins.size());
+                        coins = StringUtils.join(subCoinList.stream().map(OkxCoin::getCoin).collect(Collectors.toList()), ",");
+                        for (OkxCoin coin:subCoinList) {
+                            subBalanceList.add(new OkxAccountBalance(null,account.getId(),account.getName(),coin.getCoin(),null));
+                        }
+                    } else {
+                        subBalanceList = DbBalanceList.subList(i * 20, ((i + 1) * 20 <= DbBalanceList.size()) ? ((i + 1) * 20) : DbBalanceList.size());
+                        coins = StringUtils.join(subBalanceList.stream().map(OkxAccountBalance::getCoin).collect(Collectors.toList()), ",");
                     }
-                    JSONObject data = json.getJSONArray("data").getJSONObject(0);
-                    JSONArray detail = data.getJSONArray("details");
-                    for (int j = 0; j < detail.size(); j++) {
-                        JSONObject balance = detail.getJSONObject(j);
-                        allBalanceList.stream().filter(item -> item.getCoin().equals(balance.getString("ccy"))).findFirst().ifPresent(obj ->{
-                            obj.setBalance(balance.getBigDecimal("availBal"));
-                        });
-                    }
+                    balanceList.addAll(getBalance(coins, map, subBalanceList));
                     Thread.sleep(200);
                 }
-                balanceBusiness.saveOrUpdateBatch(allBalanceList.stream().distinct().collect(Collectors.toList()));
+                balanceBusiness.saveOrUpdateBatch(balanceList.stream().distinct().collect(Collectors.toList()));
             }
         } catch (Exception e) {
-            log.error("syncCurrencies error:", e);
+            log.error("syncOkxBalance error:", e);
         }
 
     }
 
+    private int getPages(int sum) {
+        int pages = sum / 20;
+        if (sum % 20 != 0) {
+            pages++;
+        }
+        return pages;
+    }
 
+    private List<OkxAccountBalance> getBalance(String coinsStr, Map<String, String> map,List<OkxAccountBalance> subList) {
+        String str = HttpUtil.getOkx("/api/v5/account/balance?ccy=" + coinsStr, null, map);
+        JSONObject json = JSONObject.parseObject(str);
+        if (json == null || !json.getString("code").equals("0")) {
+            log.error("str:{}", str);
+            throw new ServiceException(str);
+        }
+        JSONObject data = json.getJSONArray("data").getJSONObject(0);
+        JSONArray detail = data.getJSONArray("details");
+        for (int j = 0; j < detail.size(); j++) {
+            JSONObject balance = detail.getJSONObject(j);
+            subList.stream().filter(item -> item.getCoin().equals(balance.getString("ccy"))).findFirst().ifPresent(obj -> {
+                obj.setBalance(balance.getBigDecimal("availBal"));
+            });
+        }
+        return subList;
+    }
 
 
     @Async
